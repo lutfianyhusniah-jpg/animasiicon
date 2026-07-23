f9a71684aacd9b766f6800798112d07aeac658a2
lutfianyhusniah-jpg <lutfianyhusniah@gmail.com>
2026-07-23 09:17:30 +0700

    Refactor GSAP animations and add console logs
    Restored and organized GSAP animations for various elements, ensuring all animations are set to repeat and yoyo. Added console logs for debugging and interaction preparation.

--- script.js
+++ script.js
@@ -1,158 +1,91 @@
+// Semua animasi sudah menggunakan CSS.
+// File ini disiapkan jika nanti ingin
+// menambahkan interaksi.
 console.log("Animasi berjalan");
 console.log("Script berhasil dimuat");
+console.log(gsap);
 
-
-// TRUCK
 gsap.to(".truck", {
     x: 20,
-
     duration: 1,
-
+    
     repeat: -1,
-
     yoyo: true,
-
     ease: "power1.inOut"
 });
 
-
-// KOIN
 gsap.to(".coin", {
     y: -15,
-
     duration: 1,
-
     repeat: -1,
-
     yoyo: true,
-
-    ease: "power1.inOut"
 });
 
-
-// KALENDER
 gsap.to(".calendar", {
     rotation: 8,
-
     duration: 1,
-
     repeat: -1,
-
     yoyo: true,
-
     transformOrigin: "top center",
-
     ease: "power1.inOut"
 });
 
-
-// PESAWAT
 gsap.to(".plane", {
     x: 20,
-
     y: -10,
-
     duration: 2,
-
     repeat: -1,
-
-    yoyo: true,
-
-    ease: "power1.inOut"
+    yoyo: true
 });
 
-
-// ICON LOKASI
 gsap.to(".loc", {
     y: -8,
-
     duration: 1,
-
     repeat: -1,
-
-    yoyo: true,
-
-    ease: "power1.inOut"
+    yoyo: true
 });
 
-
-// CEKLIS
 gsap.to(".ceklis", {
     opacity: 0,
-
     duration: 0.9,
-
     repeat: -1,
-
     yoyo: true
 });
 
-
-// PAKET
 gsap.to(".paket", {
     y: -8,
-
     duration: 1.2,
-
     repeat: -1,
-
-    yoyo: true,
-
-    ease: "power1.inOut"
-});
-
-
-// BULAN
-gsap.to(".luc", {
-    y: -8,
-
-    duration: 1.2,
-
-    repeat: -1,
-
-    yoyo: true,
-
-    ease: "power1.inOut"
-});
-
-
-// KAPAL
-gsap.to(".ship", {
-    rotation: -2,
-
-    duration: 1,
-
-    repeat: -1,
-
-    yoyo: true,
-
-    transformOrigin: "top center",
-
-    ease: "power1.inOut"
-});
-
-
-// AIR
-gsap.to(".air", {
-    x: 2,
-
-    y: 0.8,
-
-    duration: 0.7,
-
-    repeat: -1,
-
     yoyo: true
 });
 
+gsap.to(".luc", {
+    y: -8,
+    duration: 1.2,
+    repeat: -1,
+    yoyo: true
+});
 
-// TULISAN
+gsap.to(".ship", {
+    rotation: -2,
+    duration: 1,
+    repeat: -1,
+    yoyo: true,
+    transformOrigin: "top center",
+    ease: "power1.inOut"
+});
+
+gsap.to(".air", {
+    x: 2,
+    y: 0.8,
+    duration: 0.7,
+    repeat: -1,
+    yoyo: true
+});
+
 gsap.to(".tulisan", {
     x: 5,
-
     duration: 1.5,
-
     repeat: -1,
-
     yoyo: true
 });
